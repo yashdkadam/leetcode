bf403090d661e002b7ba62b6eda904222ac806ce
yashdkadam <kadamdyash55@gmail.com>
2023-04-07 12:26:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function setCompany(e, data){
+  e.preventDefault();
+  localStorage.setItem('company', data);
+  window.location.reload();
+}
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [currCompany, setCurrCompany] = useState("");
+  const [companies, setCompanies] = useState([]);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const loadPost = async () => {
+      setLoading(true);
+      if (localStorage.getItem("company") == null) {
+        localStorage.setItem("company", "Amazon");
+      }
+      var base = "https://curious-parka-yak.cyclic.app/api/questions/";
+      var response = await axios.get(base + "companies");
+      setCompanies(response["data"]);
+
+      var company = localStorage.getItem("company");
+      setCurrCompany(company)
+      const res = await axios.get(base + "search/company/" + company);
+      setQuestions(res["data"]);
+      setLoading(false);
+    };
+
+    // Call the function
+    loadPost();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{localStorage.getItem("company")}</h1>
+      <div className="row">
+        <div className="col-8">
+          <ul className="list-group">
+            {loading ? (
+              <li className="list-group-item d-flex justify-content-between align-items-center">
+                Loading...
+              </li>
+            ) : (
+              questions.map(
+                (item) => (
+                  console.log(item["isPaidOnly"]),
+                  (
+                    <a
+                      href={`https://leetcode.com/problems/${item.titleSlug}`}
+                      target="_blank"
+                    >
+                      <li className="list-group-item d-flex justify-content-between align-items-center">
+                        <span>
+                          {item["title"]}
+                          {item["isPaidOnly"] ? (
+                            <span>
+                              &nbsp; &nbsp;
+                              <i class="fa fa-lock" aria-hidden="true"></i>
+                            </span>
+                          ) : null}
+                        </span>
+
+                        <span>
+                          <span
+                            class={`badge badge-pill ${item["difficulty"]}`}
+                          >
+                            {item["difficulty"]}
+                          </span>
+                          <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                          <span class="badge badge-primary badge-pill">
+                            {item["companyTags"][0]["num_occur"]}
+                          </span>
+                        </span>
+                      </li>
+                    </a>
+                  )
+                )
+              )
+            )}
+          </ul>
+        </div>
+        <div className="col">
+          <ul className="list-group">
+            {loading ? (
+              <li className="list-group-item d-flex justify-content-between align-items-center">
+                Loading
+              </li>
+            ) : (
+              companies.map((item) => (
+                <li
+                  className={
+                    item === localStorage.getItem("company")
+                      ? "list-group-item d-flex justify-content-between align-items-center active"
+                      : "list-group-item d-flex justify-content-between align-items-center"
+                  }
+                  onClick={(e) => setCompany(e, item)}
+                >
+                  {item}
+                </li>
+              ))
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
